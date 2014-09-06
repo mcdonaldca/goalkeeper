c89307d68a75d3bcd6cc714a393aa5564bdb7a5b
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2014-09-06 18:30:06 -0400

    Fixed frequency logic

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -26,22 +26,31 @@ class GoalsController < ApplicationController
 
 	def action
 		duration = params[:duration]
+		@disp = params
 		frequency = ''
-		if params[:SU] != nil
-			frequency += params[:SU]
-		elsif params[:M] != nil
-			frequency += params[:M]
-		elsif params[:TU] != nil
-			frequency += params[:TU]
-		elsif params[:W] != nil
-			frequency += params[:W]
-		elsif params[:TH] != nil
-			frequency += params[:TH]
-		elsif params[:F] != nil
-			frequency += params[:F]
-		elsif params[:SA] != nil
-			frequency += params[:SA]
+		if params[:SUN] == "1"
+			frequency += "Su"
 		end
+		if params[:MON] == "1"
+			frequency += "M"
+		end
+		if params[:TUE] == "1"
+			frequency += "Tu"
+		end
+		if params[:WED] == "1"
+			frequency += "W"
+		end
+		if params[:THU] == "1"
+			frequency += "Th"
+		end
+		if params[:FRI] == "1"
+			frequency += "F"
+		end
+		if params[:SAT] == "1"
+			frequency += "Sa"
+		end
+
+		#@disp = frequency
 		#frequency = params[:SU] + params[:M] + params[:TU] + params[:W] + params[:TH] + params[:F] + params[:SA]
 
 		if frequency == "MTuWThF"
