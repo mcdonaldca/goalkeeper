d5ba663ac11e39a54aa28718c9cc39f1d5d63356
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2014-09-06 16:10:44 -0400

    Venmo authorization! Doesn't save to user profile yet.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -28,18 +28,27 @@ class AuthController < ApplicationController
   end
 
   def venmo
+    if session[:user_id] == nil
+      flash[:notice] = "You need to login first"
+      redirect_to login_url
+    end
   end
 
   def authorization
     # Only access if user is logged in
     if !params[:access_token].nil?
-      @response = Net::HTTP.get_response(URI("https://api.venmo.com/v1/me?access_token=" + params[:access_token])).body
-      #user_info = JSON.parse(response)["data"]["user"]
-      #@access_token = params[:access_token]
-      #@username = user_info["username"]
-      
+      @token = params[:access_token]
+      uri = URI.parse("https://api.venmo.com/v1/me?access_token=" + params[:access_token])
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+
+      response = http.request(request).body
+      user_info = JSON.parse(response)["data"]["user"]
+      @access_token = params[:access_token]
+      @username = user_info["username"]      
     end
-    #redirect_to feed_url
+    redirect_to feed_url
   end
 
 end
